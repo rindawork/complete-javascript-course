34d531c7d1b2bd561727df2fee902b0c997de88b
rindawork <nirotaku@gmail.com>
2023-12-31 17:08:41 +0000

    added devcontainer

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const juancito = {
+    name: "perensejo",
+    age: 23,
+    culos: ["katy","nova",'scotia']
+}
+
+
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+console.log('worked');
+function Car(make, speed) {
+ this.make = make
+ this.speed = speed
+}
+
+Car.prototype.accelerate = function () {
+    this.speed +=10
+    console.log(this.speed);
+}
+
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(this.speed);
+}
+
+let car1 = new Car("bmw", 120);
+let car2 = new Car("Mercedes", 25)
+
+car1.brake()
+car1.accelerate()
+car2.brake()
+car2.brake()
+car2.brake()
+car2.accelerate()
